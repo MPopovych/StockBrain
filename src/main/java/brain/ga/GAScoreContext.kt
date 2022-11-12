08327965580c4a687ce78c36c5ca7ea6c99fdfc6
maxpopovych <vwotar@gmail.com>
2022-11-12 15:23:59 +0100

    concurrent array list

--- src/main/java/brain/ga/GAScoreContext.kt
+++ src/main/java/brain/ga/GAScoreContext.kt
@@ -2,9 +2,10 @@ package brain.ga
 
 import brain.ga.weights.ModelGenes
 import brain.models.Model
+import java.util.concurrent.CopyOnWriteArrayList
 
 class GAScoreContext(val generation: Int, val model: Model, val genes: ModelGenes) {
-	internal val records: ArrayList<Double> = ArrayList()
+	internal val records: CopyOnWriteArrayList<Double> = CopyOnWriteArrayList()
 	fun pushRecord(record: Double) {
 		records.add(record)
 	}
