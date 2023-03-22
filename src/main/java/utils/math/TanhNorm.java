9d21bcc97b5aaba10bb79d530f66015de71c5f69
maxpopovych <vwotar@gmail.com>
2023-03-22 16:21:46 +0100

    tanh norm

--- src/main/java/utils/math/TanhNorm.java
+++ src/main/java/utils/math/TanhNorm.java
@@ -13,16 +13,16 @@ public class TanhNorm implements ScaleImpl {
 		float[] r = new float[data.length];
 
 		float std = owner.getStd();
-		float median = owner.getMean();
+		float median = owner.getMedian();
 
 		if (std == 0f) {
-			Arrays.fill(r, 0f);
-			return r;
+			// don't allow for zero deviation
+			throw new IllegalStateException();
 		}
 
 		for (int i = 0; i < data.length; i++) {
-			float inner = (0.1f * ((data[i] - median) / std));
-			r[i] = (activation.apply(inner) + 1);
+			float inner = (0.01f * ((data[i] - median) / std));
+			r[i] = 0.5f * (activation.apply(inner) + 1);
 		}
 		return r;
 	}
@@ -30,10 +30,10 @@ public class TanhNorm implements ScaleImpl {
 	@Override
 	public float performScale(ScaleMeta owner, float data) {
 		float std = owner.getStd();
-		float median = owner.getMean();
+		float median = owner.getMedian();
 		if (std == 0f) return 0f;
 
-		float inner = (0.1f * ((data - median) / std));
-		return (activation.apply(inner) + 1);
+		float inner = (0.01f * ((data - median) / std));
+		return 0.5f * (activation.apply(inner) + 1);
 	}
 }
