d0de97ffc61a5fd9a17d931fb183b80137a89ec8
maxpopovych <vwotar@gmail.com>
2023-07-25 14:25:28 +0200

    v2 refactor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
 rootProject.name = "StockBrain"
 
+pluginManagement {
+	repositories {
+		google()
+		gradlePluginPortal()
+		mavenCentral()
+	}
 
+	plugins {
+		kotlin("jvm").version(extra["kotlin.version"] as String)
+		kotlin("plugin.serialization").version(extra["kotlin.version"] as String)
+	}
+}
