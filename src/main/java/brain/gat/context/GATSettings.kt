1f6f9fa63d992dadf67d7e959e037c24831cf856
maxpopovych <vwotar@gmail.com>
2023-07-30 18:08:23 +0200

    v2 wip

--- src/main/java/brain/gat/context/GATSettings.kt
+++ src/main/java/brain/gat/context/GATSettings.kt
@@ -5,10 +5,10 @@ import brain.gat.GATScoreBoardOrder
 class GATSettings(
 	val population: Int,
 	val topParentCount: Int,
-	val mutationRate: Float = 0.05f,
+	val mutationRate: Float = 0.02f,
 	val initialMutationRate: Float = 0.50f,
-	val weightMod: Float = 0.05f,
-	val weightSoftCap: Float = 1.0f,
+	val weightMod: Float = 0.1f,
+	val weightSoftCap: Float = 1.5f,
 	val weightHeavyCap: Float = 5.0f,
 	val clearEveryGeneration: Boolean,
 	val order: GATScoreBoardOrder,
